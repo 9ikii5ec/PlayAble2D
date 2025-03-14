bbce47b5d8a70cfd43a87f124c8db5c4740642f2
9ikii5ec <niiitakirkizh@gmail.com>
2025-03-14 21:37:24 +0300

    Reworking sprites update Build.cs

--- Assets/Scripts/Build.cs
+++ Assets/Scripts/Build.cs
@@ -1,79 +1,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class Build : MonoBehaviour
 {
     [SerializeField] private BuildGrid buildGrid;
-    [SerializeField] private float snapDistance = 0.5f;
 
-    private Camera mainCamera;
-    private Rigidbody2D rigidBody;
+    private Vector3 offset;
     private bool isDragging = false;
     private Vector2 originalPosition;
 
     private void Awake()
     {
-        mainCamera = Camera.main;
-        rigidBody = GetComponent<Rigidbody2D>();
-        originalPosition = rigidBody.position;
-
+        originalPosition = transform.position;
     }
 
-    private void Update()
+    private void OnMouseDown()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+        offset = transform.position - GetMouseWorldPosition();
+        isDragging = true;
+    }
 
-            if (hit.collider != null && hit.collider.gameObject == gameObject)
-            {
-                isDragging = true;
-            }
-        }
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition() + offset;
+    }
 
-        if (Input.GetMouseButtonUp(0))
-        {
-            isDragging = false;
-            SnapToCell();
-        }
-
-        if (isDragging)
-        {
-            Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            rigidBody.MovePosition(mousePosition);
-        }
+    private void OnMouseUp()
+    {
+        isDragging = false;
+        SnapToCell();
     }
 
     private void SnapToCell()
-{
-    List<Vector2> cellPositions = buildGrid.CellPositions;
-    Vector2 targetPosition = originalPosition;
-
-    foreach (Vector2 cellPosition in cellPositions)
     {
-        if (Vector2.Distance(rigidBody.position, cellPosition) < snapDistance)
-        {
-            Collider2D[] colliders = Physics2D.OverlapCircleAll(cellPosition, snapDistance);
-            bool isOccupied = false;
+        List<Vector2> cellPositions = buildGrid.CellPositions;
+        Vector2 targetPosition = originalPosition;
+        float minDistance = float.MaxValue;
 
-            foreach (Collider2D collider in colliders)
+        foreach (Vector2 cellPosition in cellPositions)
+        {
+            if (!IsCellOccupied(cellPosition))
             {
-                if (collider.gameObject != gameObject)
+                float distance = Vector2.Distance(transform.position, cellPosition);
+                if (distance < minDistance)
                 {
-                    isOccupied = true;
-                    break;
+                    minDistance = distance;
+                    targetPosition = cellPosition;
                 }
             }
-
-            if (!isOccupied)
-            {
-                targetPosition = cellPosition;
-                originalPosition = targetPosition;
-            }
         }
+
+        transform.position = targetPosition;
+        originalPosition = targetPosition;
     }
 
-    rigidBody.MovePosition(targetPosition);
-}
+    private bool IsCellOccupied(Vector2 cellPosition)
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(cellPosition, 0.1f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject != gameObject)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
 }
